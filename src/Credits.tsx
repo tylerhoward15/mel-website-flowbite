9f5ac5c57b55f72f418b16be7d4df59b8ecb970f
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2024-02-12 11:09:21 -0800

    implement credits

---
+++ src/Credits.tsx
@@ -0,0 +1,56 @@
+import { Button } from 'flowbite-react'
+
+function CustomCredits() {
+  return (
+    <div className="flex flex-col">
+      <h1 className="text-3xl text-center">Featured Credits</h1>
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-2">
+        <div>
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+      <Button>Full Resume</Button>
+    </div>
+  )
+}
+
+export default CustomCredits
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import './index.css'
 import CustomNavbar from './Navbar.tsx'
 import CustomHero from './Hero.tsx'
 import CustomAboutMe from './AboutMe.tsx'
+import CustomCredits from './Credits.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CustomNavbar />
     <CustomHero />
     <CustomAboutMe />
+    <CustomCredits />
   </React.StrictMode>
 )
