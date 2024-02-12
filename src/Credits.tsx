50e7d843f9742ff322605f8016ff87845acdfe0e
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2024-02-12 13:23:45 -0800

    add movie posters to credits

--- src/Credits.tsx
+++ src/Credits.tsx
@@ -1,52 +1,35 @@
 import { Button } from 'flowbite-react'
+// import annie from './assets/posters/annie-poster.jpg'
+import criminalMinds from './assets/posters/criminal-minds-poster.jpg'
+import fredClaus from './assets/posters/Fred-claus-poster.jpg'
+import goldenChristmas from './assets/posters/golden-christmas-poster.jpg'
+import vice from './assets/posters/vice-poster.jpg'
+import whiteChristmas from './assets/posters/white-christmas-poster.png'
+import youngRestless from './assets/posters/young-and-restless-poster.jpg'
 
 function CustomCredits() {
+  const posters = [
+    // annie,
+    criminalMinds,
+    fredClaus,
+    goldenChristmas,
+    vice,
+    whiteChristmas,
+    youngRestless,
+  ]
+
   return (
     <div className="flex flex-col">
       <h1 className="text-3xl text-center">Featured Credits</h1>
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-2">
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg"
-            alt=""
-          />
-        </div>
+
+      <div className="grid grid-cols-2 gap-2">
+        {posters.map((poster) => {
+          return (
+            <div key={poster}>
+              <img className="h-auto max-w-full" src={poster} alt="" />
+            </div>
+          )
+        })}
       </div>
       <Button>Full Resume</Button>
     </div>
