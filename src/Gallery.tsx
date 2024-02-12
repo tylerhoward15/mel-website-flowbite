bf2eb2492333cdd0f5f846f6e17adf3572f91ea3
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2024-02-12 14:04:18 -0800

    add gallery assets to component

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -1,52 +1,29 @@
 import { Button } from 'flowbite-react'
+const modules = await import.meta.glob(
+  ['./assets/photoshoot-pics/*.{jpg,png}'],
+  {
+    eager: true,
+    query: '?url',
+    import: 'default',
+  }
+)
+
+const galleryImages: string[] = Object.values(modules) as string[]
 
 function CustomGallery() {
   return (
     <div className="flex flex-col">
       <h1 className="text-3xl text-center">Gallery</h1>
+
       <div className="grid grid-cols-2 md:grid-cols-3 gap-2">
-        <div>
+        {galleryImages.map((image) => (
           <img
             className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
+            src={image}
             alt=""
+            key={image}
           />
-        </div>
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg"
-            alt=""
-          />
-        </div>
+        ))}
       </div>
       <Button>See More</Button>
     </div>
