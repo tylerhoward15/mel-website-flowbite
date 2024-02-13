6464ca093acd257c39d6488d118dd60311b873bd
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2024-02-12 16:04:38 -0800

    add basic styles to hero

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,9 +1,10 @@
 import { Button } from 'flowbite-react'
+import melHero from './assets/misc-melody/melody-main-hero-transparent-cropped.png'
 
 function CustomHero() {
   return (
-    <div className="flex">
-      <div className="flex-shrink">
+    <div className="">
+      <div className="relative flex flex-col h-dvh">
         <div className="z-0">
           <h1>Melody Hollis</h1>
           <h2>Actor singer dancer</h2>
@@ -11,12 +12,14 @@ function CustomHero() {
             Lets Talk
           </Button>
         </div>
-        <div className="z-0">
+        <div className="">
           <h2>Scrolling ticker * Scrolling Ticker </h2>
         </div>
-      </div>
-      <div className="flex-1 z-1">
-        <img src="/vite.svg" alt="Main picture of Melody" />
+        <img
+          className="z-1 absolute max-h-screen top-0 left-1/3 md:left-1/2"
+          src={melHero}
+          alt="Main picture of Melody"
+        />
       </div>
     </div>
   )
