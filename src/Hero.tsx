f76f4ab92f2ca33181e164b9a8ec0bddbd8beba7
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2024-02-12 23:58:16 -0800

    more ticker style improvements

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -7,7 +7,7 @@ const ticks = ['Actor', 'Singer', 'Dancer', 'Teacher', 'Broadway', 'Theater']
 
 function CustomHero() {
   return (
-    <div className="relative bg-gradient-to-r from-gray-500 to-gray-400 md:grid md:grid-cols-2 p-3 pb-0 font-bold">
+    <div className="relative bg-gradient-to-r from-gray-500 to-gray-400 md:grid md:grid-cols-2 p-3 pb-0 font-bold overflow-hidden">
       <div className="">
         <h2 className="text-[#f8f9fa]">Melody Hollis</h2>
         <h2 className="text-[#212529]">Actor, Singer, Dancer</h2>
@@ -16,15 +16,17 @@ function CustomHero() {
           <HiOutlineArrowRight className="ml-2 h-5 w-5" />
         </Button>
       </div>
-      <div className="text-[#f8f9fa] row-start-2 absolute top-1/2 text-clip inline-flex">
-        {ticks.map((tick) => {
-          return (
-            <div className="flex-none">
-              <h2 className="inline p-5 md:p-10">{tick}</h2>
-              <img className="inline" src={stars}></img>
-            </div>
-          )
-        })}
+      <div className="text-[#f8f9fa] row-start-2 absolute top-1/2">
+        <div className="inline-flex w-screen">
+          {ticks.map((tick) => {
+            return (
+              <div className="flex-none" key={tick}>
+                <h2 className="inline p-5 md:p-10">{tick}</h2>
+                <img className="inline" src={stars}></img>
+              </div>
+            )
+          })}
+        </div>
       </div>
       <img
         className="z-50 row-span-2 relative"
