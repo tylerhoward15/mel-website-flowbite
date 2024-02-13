c01a3db5c01f621ec236db3dad969d708506a1b9
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2024-02-12 22:25:27 -0800

    pick specific photos for gallery

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -1,12 +1,20 @@
 import { Button } from 'flowbite-react'
 import starsWarning from './assets/stars-warning.svg'
-const modules = import.meta.glob(['./assets/photoshoot-pics/*.{jpg,png}'], {
-  eager: true,
-  query: '?url',
-  import: 'default',
-})
+import fiveThreeNine from './assets/photoshoot-pics/MelodyHollis-539.jpg'
+import sixSevenNine from './assets/photoshoot-pics/MelodyHollis-679.jpg'
+import sevenOneZero from './assets/photoshoot-pics/MelodyHollis-710.jpg'
+import fourTwoNine from './assets/photoshoot-pics/MelodyHollis-429.jpg'
+import oneNineEight from './assets/photoshoot-pics/MelodyHollis-198.jpg'
+import fiveNineNine from './assets/photoshoot-pics/MelodyHollis-599.jpg'
 
-const galleryImages: string[] = Object.values(modules) as string[]
+const galleryImages = [
+  fiveThreeNine,
+  sixSevenNine,
+  sevenOneZero,
+  fourTwoNine,
+  oneNineEight,
+  fiveNineNine,
+]
 
 function CustomGallery() {
   return (
@@ -15,7 +23,7 @@ function CustomGallery() {
       <h2 className="text-center underline m-5">Gallery</h2>
 
       <div className="grid grid-cols-2 md:grid-cols-3 gap-5 md:gap-10 bg-slate-200 p-5 md:p-10 rounded-xl">
-        {galleryImages.slice(0, 6).map((image) => (
+        {galleryImages.map((image) => (
           <img
             className="h-auto max-w-full rounded-lg shadow-2xl transition ease-in-out delay-15 hover:-translate-y-1 hover:scale-110 duration-300"
             src={image}
@@ -23,7 +31,7 @@ function CustomGallery() {
             key={image}
           />
         ))}
-        <Button outline gradientDuoTone="purpleToPink" className="m-5">
+        <Button outline gradientDuoTone="purpleToPink" className="">
           See More
         </Button>
       </div>
