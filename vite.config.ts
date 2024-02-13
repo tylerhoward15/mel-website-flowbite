4819445e6076a3747175c3d949160d359b1288ae
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2024-02-12 21:24:03 -0800

    add imagemin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+      jpeg: {
+        quality: 20,
+      },
+      jpg: {
+        quality: 20,
+      },
+    }),
+  ],
 })
