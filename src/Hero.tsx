6762f81bb8dbe68fcebe635174c436e2f67535df
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2024-02-13 00:02:41 -0800

    improve padding/spacing on hero

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -8,7 +8,7 @@ const ticks = ['Actor', 'Singer', 'Dancer', 'Teacher', 'Broadway', 'Theater']
 function CustomHero() {
   return (
     <div className="relative bg-gradient-to-r from-gray-500 to-gray-400 md:grid md:grid-cols-2 p-3 pb-0 font-bold overflow-hidden">
-      <div className="">
+      <div className="md:p-10">
         <h2 className="text-[#f8f9fa]">Melody Hollis</h2>
         <h2 className="text-[#212529]">Actor, Singer, Dancer</h2>
         <Button outline gradientDuoTone="purpleToPink" className="my-3">
@@ -16,7 +16,7 @@ function CustomHero() {
           <HiOutlineArrowRight className="ml-2 h-5 w-5" />
         </Button>
       </div>
-      <div className="text-[#f8f9fa] row-start-2 absolute top-1/2">
+      <div className="text-[#f8f9fa] row-start-2 absolute top-1/2 md:top-1/4">
         <div className="inline-flex w-screen">
           {ticks.map((tick) => {
             return (
