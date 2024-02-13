791180578d4dda5d77d5098e3a571326a6b7416b
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2024-02-12 19:50:12 -0800

    refactor and expand hero component

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,26 +1,26 @@
 import { Button } from 'flowbite-react'
 import melHero from './assets/misc-melody/melody-main-hero-transparent-cropped.png'
+import { HiOutlineArrowRight } from 'react-icons/hi'
 
 function CustomHero() {
   return (
-    <div className="">
-      <div className="relative flex flex-col h-dvh">
-        <div className="z-0">
-          <h1>Melody Hollis</h1>
-          <h2>Actor singer dancer</h2>
-          <Button pill outline>
-            Lets Talk
-          </Button>
-        </div>
-        <div className="">
-          <h2>Scrolling ticker * Scrolling Ticker </h2>
-        </div>
-        <img
-          className="z-1 absolute max-h-screen top-0 left-1/3 md:left-1/2"
-          src={melHero}
-          alt="Main picture of Melody"
-        />
+    <div className="bg-gradient-to-r from-gray-500 to-gray-400 md:grid md:grid-cols-2 p-3 pb-0">
+      <div className="z-0 text-2xl">
+        <h1 className="text-[#f8f9fa]">Melody Hollis</h1>
+        <h2>Actor, Singer, Dancer</h2>
+        <Button outline gradientDuoTone="purpleToPink" className="my-3">
+          Let's Talk
+          <HiOutlineArrowRight className="ml-2 h-5 w-5" />
+        </Button>
       </div>
+      <div className="text-[#f8f9fa] row-start-2 text-4xl">
+        <h2>Scrolling Ticker * Scrolling Ticker </h2>
+      </div>
+      <img
+        className="z-1 row-span-2"
+        src={melHero}
+        alt="Main picture of Melody"
+      />
     </div>
   )
 }
