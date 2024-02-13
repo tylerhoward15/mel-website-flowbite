0f0e06a051dc4af6e400d1f652b3f0eaf0e0ac7d
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2024-02-12 22:18:04 -0800

    improve gallery style

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -14,19 +14,19 @@ function CustomGallery() {
       <img src={starsWarning} alt="" />
       <h2 className="text-center underline m-5">Gallery</h2>
 
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-2">
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-5 md:gap-10 bg-slate-200 p-5 md:p-10 rounded-xl">
         {galleryImages.slice(0, 6).map((image) => (
           <img
-            className="h-auto max-w-full rounded-lg shadow-2xl"
+            className="h-auto max-w-full rounded-lg shadow-2xl transition ease-in-out delay-15 hover:-translate-y-1 hover:scale-110 duration-300"
             src={image}
             alt=""
             key={image}
           />
         ))}
+        <Button outline gradientDuoTone="purpleToPink" className="m-5">
+          See More
+        </Button>
       </div>
-      <Button outline gradientDuoTone="purpleToPink" className="m-5">
-        See More
-      </Button>
     </div>
   )
 }
